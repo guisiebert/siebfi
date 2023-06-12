3da31e1776d4caf519782c24626f705a5b909f89
Gui Siebert <guilhermebsiebert@gmail.com>
2023-06-12 19:08:17 -0300

    initiated dialog creation with radix-ui

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,20 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logo from '../../assets/logo.svg'
+import * as Dialog from '@radix-ui/react-dialog'
 
 export function Header() {
     return (
         <HeaderContainer>
             <HeaderContent>
                 <img src={logo} alt="SiebFi Logo" />
-                <NewTransactionButton> Nova transação</NewTransactionButton>
+
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                        <NewTransactionButton> Nova transação</NewTransactionButton>
+                    </Dialog.Trigger>
+
+                </Dialog.Root>
+
             </HeaderContent>
         </HeaderContainer>
     )
